60ba58c47c04bea04dae3f18cf7243dec2605126
Dea Shafira A <89475793+deashfra@users.noreply.github.com>
2024-07-03 22:56:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,151 @@
 import streamlit as st
 import pandas as pd
+import math
+from pathlib import Path
 
-# Judul aplikasi
-st.title('Infografis Interaktif Desa Pardomuan')
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='GDP dashboard',
+    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+)
 
-# Data Geografi
-st.header('Geografi Kecamatan Sitellu Tali Urang Julu')
-st.write('Luas kecamatan: 53,20 km²')
+# -----------------------------------------------------------------------------
+# Declare some useful functions.
 
-data_luas = {
-    'Desa': ['Silima Kuta', 'Ulu Merah', 'Pardomuan', 'Lae Langge Namuseng', 'Cikaok'],
-    'Luas (%)': [16.8, 40.2, 19.6, 13.7, 9.7]
-}
-df_luas = pd.DataFrame(data_luas)
-st.write('Luas Area Menurut Desa (%)')
-st.bar_chart(df_luas.set_index('Desa'))
+@st.cache_data
+def get_gdp_data():
+    """Grab GDP data from a CSV file.
 
-# Jarak ke Ibukota Kecamatan
-st.header('Jarak ke Ibukota Kecamatan Menurut Desa/Kelurahan')
-data_jarak = {
-    'Desa': ['Silima Kuta', 'Ulu Merah', 'Pardomuan', 'Lae Langge Namuseng', 'Cikaok'],
-    'Jarak (km)': [3, 0, 5, 3, 5]
-}
-df_jarak = pd.DataFrame(data_jarak)
-st.write('Jarak (km)')
-st.bar_chart(df_jarak.set_index('Desa'))
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
 
-# Nama Dusun Menurut Desa
-st.header('Nama Dusun Menurut Desa')
-data_dusun = {
-    'Silima Kuta': ['Dusun I Kuta Kersik', 'Dusun II Kuta Kersik', 'Dusun I Singgabur', 'Dusun II Singgabur'],
-    'Ulu Merah': ['Dusun I Ulu Merah', 'Dusun II Ulu Merah', 'Dusun III Maneas'],
-    'Pardomuan': ['Dusun I Pardomuan', 'Dusun II Pardomuan', 'Dusun III Lae Mbulan'],
-    'Lae Langge Namuseng': ['Dusun I Lae Langge', 'Dusun II Namuseng', 'Dusun III Lae Rambung', 'Dusun IV Lae Bunga'],
-    'Cikaok': ['Dusun I Cikaok', 'Dusun II Lebbuh', 'Dusun III Sosor', 'Dusun IV Persabahen']
-}
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
+    raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
-for desa, dusun in data_dusun.items():
-    st.subheader(desa)
-    for d in dusun:
-        st.write(d)
+    MIN_YEAR = 1960
+    MAX_YEAR = 2022
+
+    # The data above has columns like:
+    # - Country Name
+    # - Country Code
+    # - [Stuff I don't care about]
+    # - GDP for 1960
+    # - GDP for 1961
+    # - GDP for 1962
+    # - ...
+    # - GDP for 2022
+    #
+    # ...but I want this instead:
+    # - Country Name
+    # - Country Code
+    # - Year
+    # - GDP
+    #
+    # So let's pivot all those year-columns into two: Year and GDP
+    gdp_df = raw_gdp_df.melt(
+        ['Country Code'],
+        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
+        'Year',
+        'GDP',
+    )
+
+    # Convert years from string to integers
+    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+
+    return gdp_df
+
+gdp_df = get_gdp_data()
+
+# -----------------------------------------------------------------------------
+# Draw the actual page
+
+# Set the title that appears at the top of the page.
+'''
+# :earth_americas: GDP dashboard
+
+Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
+notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
+But it's otherwise a great (and did I mention _free_?) source of data.
+'''
+
+# Add some spacing
+''
+''
+
+min_value = gdp_df['Year'].min()
+max_value = gdp_df['Year'].max()
+
+from_year, to_year = st.slider(
+    'Which years are you interested in?',
+    min_value=min_value,
+    max_value=max_value,
+    value=[min_value, max_value])
+
+countries = gdp_df['Country Code'].unique()
+
+if not len(countries):
+    st.warning("Select at least one country")
+
+selected_countries = st.multiselect(
+    'Which countries would you like to view?',
+    countries,
+    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+
+''
+''
+''
+
+# Filter the data
+filtered_gdp_df = gdp_df[
+    (gdp_df['Country Code'].isin(selected_countries))
+    & (gdp_df['Year'] <= to_year)
+    & (from_year <= gdp_df['Year'])
+]
+
+st.header('GDP over time', divider='gray')
+
+''
+
+st.line_chart(
+    filtered_gdp_df,
+    x='Year',
+    y='GDP',
+    color='Country Code',
+)
+
+''
+''
+
+
+first_year = gdp_df[gdp_df['Year'] == from_year]
+last_year = gdp_df[gdp_df['Year'] == to_year]
+
+st.header(f'GDP in {to_year}', divider='gray')
+
+''
+
+cols = st.columns(4)
+
+for i, country in enumerate(selected_countries):
+    col = cols[i % len(cols)]
+
+    with col:
+        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+
+        if math.isnan(first_gdp):
+            growth = 'n/a'
+            delta_color = 'off'
+        else:
+            growth = f'{last_gdp / first_gdp:,.2f}x'
+            delta_color = 'normal'
+
+        st.metric(
+            label=f'{country} GDP',
+            value=f'{last_gdp:,.0f}B',
+            delta=growth,
+            delta_color=delta_color
+        )
